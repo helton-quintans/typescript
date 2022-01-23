a881c77663d0137a1b2487fefadfde19ed983216
Helton Quintãns <helton.quit@gmail.com>
2022-01-23 00:27:35 -0300

    add get route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import express from 'express'
+import express, { response } from 'express'
 
 const app = express()
+
+app.get('/', (res, req)=>{
+
+    return response.json({ message: 'Hellow world'})
+})
 
 app.listen(3333)
